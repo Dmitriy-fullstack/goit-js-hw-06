62cb6fdb58745b9a69ff26ce68cdaff67f364a0f
Dmitriy Galochkin <arenda.tehniki87@gmail.com>
2020-09-17 17:31:10 +0300

    task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,38 +1,43 @@
-class StringBuilder {
-  constructor(value){
-  this._value = value;
-  }
-  
-  get value(){
-  return this._value; 
-  }
-  
-  append(str){
-   this._value += str;
-  }
-  
-  prepend(str){
-   this._value = str + this._value;
-  }
-  
-  pad(str){
-   this.prepend(str) + this.append(str)
-   }
-  
-}  
+// Write code under this line
+const getInactiveUsers = array  => array.filter(({isActive}) => isActive !== true);
 
- console.log(typeof StringBuilder);
-// 'function'
+ console.log(getInactiveUsers(users));
 
-
- const builder = new StringBuilder('.');
-
- builder.append('^');
- console.log(builder.value); // '.^'
-
- builder.prepend('^');
- console.log(builder.value); // '^.^'
-
- builder.pad('=');
- console.log(builder.value); // '=^.^='
-
+/* [
+  {
+    id: '701b29c3-b35d-4cf1-a5f6-8b12b29a5081',
+    name: 'Moore Hensley',
+    email: 'moorehensley@indexia.com',
+    eyeColor: 'blue',
+    friends: ['Sharron Pace'],
+    isActive: false,
+    balance: 2811,
+    skills: ['ipsum', 'lorem'],
+    gender: 'male',
+    age: 37,
+  },
+  {
+    id: '88beb2f3-e4c2-49f3-a0a0-ecf957a95af3',
+    name: 'Ross Vazquez',
+    email: 'rossvazquez@xinware.com',
+    eyeColor: 'green',
+    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+    isActive: false,
+    balance: 3793,
+    skills: ['nulla', 'anim', 'proident', 'ipsum', 'elit'],
+    gender: 'male',
+    age: 24,
+  },
+  {
+    id: '150b00fb-dd82-427d-9faf-2879ea87c695',
+    name: 'Blackburn Dotson',
+    email: 'blackburndotson@furnigeer.com',
+    eyeColor: 'brown',
+    friends: ['Jacklyn Lucas', 'Linda Chapman'],
+    isActive: false,
+    balance: 1498,
+    skills: ['non', 'amet', 'ipsum'],
+    gender: 'male',
+    age: 38,
+  }     
+]; */
