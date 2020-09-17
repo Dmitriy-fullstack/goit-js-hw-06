440ad32cc8d7475058e079a17fe65223a68fe39a
Dmitriy Galochkin <arenda.tehniki87@gmail.com>
2020-09-17 23:38:54 +0300

    task-8

--- js/task-8.js
+++ js/task-8.js
@@ -1,16 +1,11 @@
-function reduceArray(array) {
-    'use strict';
-    let total = 0;
-    // Write code under this line
-    for(let i=0; i<array.length; i++) {
-      total += array[i];
-    } 
-    return total;
-  };
-  
-  console.log(reduceArray([1, 2, 3]));
-  // 6
-  
-  console.log(reduceArray([-2, 0, 2]));
-  // 0
+// Write code under this line
+const getUsersWithFriend = (array, friendName) => array
+.filter(({friends}) => friends.includes(friendName))
+.map(({name}) => name);
+
+ console.log(getUsersWithFriend(users, 'Briana Decker'));
+// [ 'Sharlene Bush', 'Sheree Anthony' ]
+
+ console.log(getUsersWithFriend(users, 'Goldie Gentry'));
+// [ 'Elma Head', 'Sheree Anthony' ]
   
